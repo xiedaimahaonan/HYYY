8d50007ce9114e8e6599ccc415e6719213697b69
HJ-ZJ <1964219248@qq.com>
2023-09-25 11:35:11 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-2gozghui50619c35","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"C:\Using\wechat\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-2gozghui50619c35 --n quickstartFunctions --r --project "C:\Bc\xiaoc" --report_first --report
